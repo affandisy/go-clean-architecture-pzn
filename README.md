a047c113a83b29c38aa28b033c8b0002271f7c75
affandisy <syihabuddin.affandi@gmail.com>
2026-01-01 20:57:11 +0700

    feat(go):menggunakan golang database migrasi

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # belajar clean architecture dari pzn
+
+```bash
+go run cmd/web/main.go
+```
+
+### Membuat Migrasi
+
+```shell
+migrate create -ext sql -dir db/migrations create_table_xxx
+```
+
+### Menjalankan Migrasi
+```shell
+migrate -database "mysql://root:@tcp(localhost:3306)/golang_clean_architecture?charset=utf8mb4&parseTime=True&loc=Local" -path db/migrations up
+```
--- db/migrations/00000000001_create_users_table.up.sql
+++ db/migrations/00000000001_create_users_table.up.sql
@@ -3,8 +3,8 @@ create table users (
     name       varchar(100) not null,
     password   varchar(100) not null,
     token      varchar(100) null,
-    created_at bigint       not null default current_timestamp,
-    updated_at bigint       not null default current_timestamp on update current_timestamp,
+    created_at bigint not null,
+    updated_at bigint not null,
     primary key(id)
 ) engine = InnoDB;
 
@@ -15,3 +15,5 @@ create table users (
 --     created_at timestamp default current_timestamp not null,
 --     updated_at timestamp default current_timestamp on update current_timestamp not null,
 --     constraint username_unique unique (username),
+    -- created_at bigint       not null default current_timestamp,
+    -- updated_at bigint       not null default current_timestamp on update current_timestamp,
